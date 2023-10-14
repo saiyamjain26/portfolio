70019c29c5bf30c44293df5fa05816c33be35751
Saiyam Jain <85358097+saiyamjain26@users.noreply.github.com>
2023-10-14 13:18:04 +0530

    Add files via upload

---
+++ src/components/ExperienceAndEducation.js
@@ -0,0 +1,107 @@
+function ExperienceAndEducation() {
+  return (
+    <div className="contain">
+      <h1>Experience And Education</h1>
+      <hr />
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <h5>
+                MERN Full Stack Internship Program (August 2023 - Present)
+              </h5>
+              Ethnus Codemithra
+            </td>
+          </tr>
+          <hr />
+          <tr>
+            <td>
+              <h5>
+                Java Full Stack Internship (August 2023 - Present)
+              </h5>
+              IamNeo
+            </td>
+          </tr>
+          <hr />
+          <tr>
+            <td>
+              <h5>Content Creator (March 2023 - Present)</h5>
+              Voice-IT
+            </td>
+          </tr>
+          <hr />
+          <tr>
+            <td>
+              <br />
+              Engaged in social media content creation and poster design for
+              cross-platform distribution and assisted in creating worksheets
+              for students.
+            </td>
+          </tr>
+          <hr />
+          <tr>
+            <td>
+            <h5>ETHNUS INTERNSHIP FIRST TASKS</h5>
+              CSS AND HTML TASKS OF ETHNUS INTERNSHIP
+              <br />
+              <a
+                href="https://github.com/saiyamjain26/EthnusInternship"
+                className="noDecore"
+              >
+                GitHub Repository
+              </a>
+            </td>
+          </tr>
+          <hr />
+          <tr>
+            <td>
+              <h5>Bachelor of Technology (2021-Present)</h5>
+              Vellore Institute of Technology, Chennai
+              <br />
+              Current CGPA (for 5 semesters): 8.82
+            </td>
+          </tr>
+          <hr />
+          <tr>
+            <td>
+              <h5>
+                Senior Secondary Education (XII<sup>th</sup> Grade) : CBSE Board
+                (2021)
+              </h5>
+              Lucknow Public School, Sector-D, Lucknow
+              <br />
+              Percentage: 83.6%
+            </td>
+          </tr>
+          <hr />
+          <tr>
+            <td>
+              <h5>ETHNUS INTERNSHIP SECOND TASKS</h5>
+              BOOTSTRAP AND JS TASKS OF ETHNUS INTERNSHIP
+              <br />
+              <a
+                href="https://github.com/saiyamjain26/EthnusInternship2"
+                className="noDecore"
+              >
+                GitHub Repository
+              </a>
+            </td>
+          </tr>
+          <hr />
+          <tr>
+            <td>
+              <h5>
+                Secondary Education (X<sup>th</sup> Grade) : CBSE Board (2019)
+              </h5>
+              Lucknow Public School, Sector-D, Lucknow
+              <br />
+              Percentage: 96.2%
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default ExperienceAndEducation;
